3dd0927870ccd259b51432590a5efb77c05feb69
KimHyunJun <kimhyunjun@KimHyunJunui-MacBookAir.local>
2023-03-10 16:58:45 +0900

    #Java-백엔드-3기-git-실습/1 스켈레톤 작성

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input : ");
+
+        System.out.println("result : ");
+    }
+
+    public static int add(int a, int b) {
+        return 0;
+    }
+
+    public static int sub(int a, int b) {
+        return 0;
+    }
+
+    public static int duv(int a, int b) {
+        return 0;
+    }
+
+    public static int mul(int a, int b) {
+        return 0;
+    }
+
+    public static int mod(int a, int b) {
+        return 0;
+    }
+
+}
